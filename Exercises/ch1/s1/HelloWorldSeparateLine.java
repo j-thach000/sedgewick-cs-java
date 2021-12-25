7dc6c4056b7e9f94c9e6c2ba3bbe8d16f6e1e154
j-thach000 <jamesthach64@gmail.com>
2021-12-25 17:29:34 -0500

    Complete exercises for ch1 s1

---
+++ Exercises/ch1/s1/HelloWorldSeparateLine.java
@@ -0,0 +1,8 @@
+public class HelloWorldSeparateLine {
+    public static void main(String[] args) {
+        System.out.println("Hello,
+                           World");
+                           // Putting it on another line results in an illegal line end
+                           // It doesn't compile either
+    }
+}
---
+++ Exercises/ch1/s1/MisspellHelloWorld.java
@@ -0,0 +1,10 @@
+public class MisspellHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        // What happens when I misspell:
+        // a. public: compile error
+        // b. static: compile error
+        // c. void: compile error
+        // d. args: correctly compiled and executed
+    }
+}
