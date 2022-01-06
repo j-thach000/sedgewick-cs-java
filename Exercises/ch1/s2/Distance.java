6621a5f8357f7a29cfdfbdffc015604684a654b2
j-thach000 <jamesthach64@gmail.com>
2022-01-06 15:31:47 -0500

    Completion of additional exercises

---
+++ Exercises/ch1/s2/Distance.java
@@ -0,0 +1,10 @@
+public class Distance {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int origin = 0;
+        int distX = x - 0;
+        int distY = y - 0;
+        System.out.println(distX + "," + distY);
+    }
+}
--- Exercises/ch1/s2/Statements.java
+++ Exercises/ch1/s2/Statements.java
@@ -1,16 +1,14 @@
 public class Statements {
     public static void main(String[] args) {
+        int a = 1;
+        int b = a;
 
-        int a, b;
-        a = 1;
-        b = 2;
-        int before = (a + b);
-        System.out.println(before);
         int t = a;
-        a = b;
         b = t;
-        int after = (a + b);
-        System.out.println(after);
+        a = b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(t);
 
     }
 }
