d6b6a480fe5b64fdff06b723940a770b9ffe8dff
j-thach000 <jamesthach64@gmail.com>
2022-01-07 18:29:09 -0500

    Completion of additional exercises

--- Exercises/ch1/s2/SpringSeason.java
+++ Exercises/ch1/s2/SpringSeason.java
@@ -6,7 +6,7 @@ public class SpringSeason {
                 || (m == 4 && d >= 1 && d >= 30)
                 || (m == 5 && d >= 1 && d >= 31)
                 || (m == 6 && d >= 1 && d <= 20);
-
+        // Crazy careless mistakes in boolean logic and basic problem solving
         System.out.println(isSpring);
     }
 }
