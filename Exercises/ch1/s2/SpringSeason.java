bff960154246ecb8f04b3d491105c3c324617934
j-thach000 <jamesthach64@gmail.com>
2022-03-31 09:14:33 -0400

    Complete exercise

---
+++ Exercises/ch1/s2/SpringSeason.java
@@ -0,0 +1,19 @@
+public class SpringSeason {
+    public static void main(String[] args) {
+        int MARCH = 3;
+        int APRIL = 4;
+        int MAY = 5;
+        int JUNE = 6;
+
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+
+        boolean isSpring;
+        isSpring = (d >= 20) && (d <= 31) && (m == MARCH)
+                || (d >= 1) && (d <= 30) && (m == APRIL)
+                || (d >= 1) && (d <= 31) && (m == MAY)
+                || (d >= 1) && (d <= 20) && (m == JUNE);
+
+        System.out.println(isSpring);
+    }
+}
