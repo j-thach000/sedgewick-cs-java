d38e515be1c686acad13fc9bf1756f08a5bc3875
j-thach000 <jamesthach64@gmail.com>
2022-04-11 23:36:10 -0400

    Add completed exercise

---
+++ Exercises/ch1/s4/HugeArray.java
@@ -0,0 +1,7 @@
+public class HugeArray {
+    public static void main(String[] args) {
+    int n = 1000;
+    int[] a = new int[n*n*n*n];
+    // run time error that looks like integer overflow
+    }
+}
