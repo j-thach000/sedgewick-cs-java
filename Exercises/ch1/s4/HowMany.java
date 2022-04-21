69dc82720688df3c0394def882648364b09817fa
j-thach000 <jamesthach64@gmail.com>
2022-04-20 23:55:04 -0400

    Add completed exercise

---
+++ Exercises/ch1/s4/HowMany.java
@@ -0,0 +1,7 @@
+public class HowMany {
+    public static void main(String[] args) {
+        // allows the array length to be measured by the amount of arguments
+        int n = args.length;
+        System.out.println("Arguments Inputted: " + args.length);
+    }
+}
