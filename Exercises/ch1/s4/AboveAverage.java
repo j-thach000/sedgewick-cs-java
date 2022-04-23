af856de500f2e652174772d1614ff15eab54389b
j-thach000 <jamesthach64@gmail.com>
2022-04-22 23:44:10 -0400

    Progress on AboveAverage exercise

---
+++ Exercises/ch1/s4/AboveAverage.java
@@ -0,0 +1,10 @@
+public class AboveAverage {
+    public static void main(String[] args) {
+        int n = args.length;
+        for (int i = 0; i < args.length; i++)
+        {
+            int x = Integer.parseInt(args[i]);
+        }
+
+    }
+}
