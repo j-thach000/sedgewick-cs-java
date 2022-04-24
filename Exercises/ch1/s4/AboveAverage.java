b054708a66ae93cd452910c1f20ce735378cecc2
j-thach000 <jamesthach64@gmail.com>
2022-04-24 17:49:11 -0400

    Add completed exercise

--- Exercises/ch1/s4/AboveAverage.java
+++ Exercises/ch1/s4/AboveAverage.java
@@ -1,10 +1,16 @@
 public class AboveAverage {
     public static void main(String[] args) {
-        int n = args.length;
-        for (int i = 0; i < args.length; i++)
-        {
+        int AVERAGE = Integer.parseInt(args[0]); // average
+        int aboveAverage = 0; // array values above average
+
+        for (int i = 1; i < args.length; i++)
+        { // converts array value to int and compares against average
             int x = Integer.parseInt(args[i]);
+            if (x > AVERAGE) aboveAverage++;
         }
 
+        System.out.println("Average      : " + args[0]);
+        System.out.print("Above Average: ");
+        System.out.println(aboveAverage + " / " + (args.length - 1));
     }
 }
